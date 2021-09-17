824c556a6860aa0be3264547124e58ea649fb23b
Juliano-hub <juliano.buss@gmail.com>
2021-09-17 16:49:59 -0300

    Add files via upload

--- MatrizMenor.py
+++ MatrizMenor.py
@@ -2,9 +2,15 @@ A = []
 for i in range(5):
     A.append( [7] * 5 )
     
-A[4][4] = 5
+A[4][1] = 5
 A[0][4] = 4
 
-print(A)
-print(min(A[0]))
-print(min(A[4]))
+print(f'\nMatriz: {A}')
+
+min0 = min(A[0])
+print(f'Minimo do A[0][x]: {min0}')
+print(f'Posicao do minimo de A[0][x]: {A[0].index(min0)}')
+
+min4 = min(A[4])
+print(f'Minimo do A[4][x]: {min4}')
+print(f'Posicao do minimo de A[4][x]: {A[4].index(min4)}')
